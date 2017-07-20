bdc29326c6a9aa1c3f8b8096632df799fb0c50c9
Bart Webber, Maria Thomas and Paul Guevarra <maria@>
2017-07-20 14:33:07 -0700

    add new player array to add new players

--- js/scripts-test3.js
+++ js/scripts-test3.js
@@ -1,9 +1,21 @@
 // Back end
+function PigDicePlayers = function (){
+  this.Players = [];
+};
+
+PigDicePlayers.prototype.addPlayer = function (Player) {
+  this.Players.push(Player);
+  Player.id = this.Players.length;
+  return this;
+};
+
 function Player(name, score, roll) {
   this.name = name;
   this.score = 0;
   this.roll = 0;
-}
+  this.id = null;
+  return alert('Player' + this.name + ' has been created');
+};
 
 var rollDice = function() {
   return Math.ceil(Math.random()*6);
@@ -33,6 +45,11 @@ $(document).ready(function(){
     var newPlayer3 = new Player(inputtedPlayerName);
     $(".player3Name").text(newPlayer3.name);
 
+  $("#play-button").click(function(event){
+    event.preventDefault();
+    $("#player-name").hide();
+
+
     $("button#roll-button").click(function(event){
       event.preventDefault();
 
@@ -44,32 +61,33 @@ $(document).ready(function(){
     // alert(newPlayer.roll);
       if (roll === 1){
         alert("1 Rolled..... turn over");
-        alert("Computer Turn");
-        $("#player1panel").hide();
-        $("#computer-panel").show();
-        roll = rollDice();
-        $("span#result2").text(roll);
-        if (roll === 1){
-          computerTempScore = 0;
-          if (roll === 1){
-            alert("1 Rolled..... turn over");
-          }
-        }
-        alert(roll);
-        computerTempScore = computerTempScore + roll;
-        alert(computerTempScore);
-        roll = rollDice();
-        if (roll === 1){
-          computerTempScore = 0;
 
-          if (roll === 1){
-            alert("1 Rolled..... turn over");
-          }
-        }
-        alert(roll);
-        computerTempScore2 = computerTempScore + roll;
-        alert(computerTempScore2);
-        alert(newPlayer.name + "Your Turn");
+        // alert("Computer Turn");
+        // $("#player1panel").hide();
+        // $("#computer-panel").show();
+        // roll = rollDice();
+        // $("span#result2").text(roll);
+        // if (roll === 1){
+        //   computerTempScore = 0;
+        //   if (roll === 1){
+        //     alert("1 Rolled..... turn over");
+        //   }
+        // }
+        // alert(roll);
+        // computerTempScore = computerTempScore + roll;
+        // alert(computerTempScore);
+        // roll = rollDice();
+        // if (roll === 1){
+        //   computerTempScore = 0;
+        //
+        //   if (roll === 1){
+        //     alert("1 Rolled..... turn over");
+        //   }
+        // }
+        // alert(roll);
+        // computerTempScore2 = computerTempScore + roll;
+        // alert(computerTempScore2);
+        alert(newPlayer2.name + "Your Turn");
         $("#player1panel").show();
         $("#computer-panel").hide();
 
@@ -77,7 +95,7 @@ $(document).ready(function(){
     } else {
       newPlayer.roll += roll;
     }
-    $("span#tempScore").text(newPlayer.roll);
+    $("span#tempScore2").text(newPlayer2.roll);
     });
     $("button#hold-button").click(function(event){
       event.preventDefault();
@@ -86,32 +104,34 @@ $(document).ready(function(){
       $("span#playerScore").text(newPlayer.score);
       if (newPlayer.score >= 100){
         alert(newPlayer.name + " WINS!!!");
-      }
-      alert("Computer Turn");
-      $("#player1panel").hide();
-      $("#computer-panel").show();
-      roll = rollDice();
-      $("span#result2").text(roll);
+      } else alert(NewPlayer.name)
 
-      var turn = 0;
-      var maxTurn = rollDice();
-
-      while ((roll !==1) && (turn <=maxTurn)) {
-        computerTempScore = computerTempScore + roll;
-        alert(roll);
-        alert(computerTempScore);
-        roll = rollDice();
-        if (roll ===1) {
-          computerTempScore = 0;
-        }
-        turn +=1;
-      }
-      console.log(computerTempScore);
+      // alert("Computer Turn");
+      // $("#player1panel").hide();
+      // $("#computer-panel").show();
+      // roll = rollDice();
+      // $("span#result2").text(roll);
+      //
+      // var turn = 0;
+      // var maxTurn = rollDice();
+      //
+      // while ((roll !==1) && (turn <=maxTurn)) {
+      //   computerTempScore = computerTempScore + roll;
+      //   alert(roll);
+      //   alert(computerTempScore);
+      //   roll = rollDice();
+      //   if (roll ===1) {
+      //     computerTempScore = 0;
+      //   }
+      //   turn +=1;
+      // }
+      // console.log(computerTempScore);
 
       alert(newPlayer.name + "Your Turn");
       $("#player1panel").show();
       $("#computer-panel").hide();
       });
+  });
   });
   });
   });
